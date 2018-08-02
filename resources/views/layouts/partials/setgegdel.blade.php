bd7df5e684f37ed3f7d5fbf3ebe8318d0442e4c7
Purevdorj <ncst.puje@gmail.com>
2018-08-02 11:58:31 +0800

    from pc

--- resources/views/layouts/partials/setgegdel.blade.php
+++ resources/views/layouts/partials/setgegdel.blade.php
@@ -1,12 +1,15 @@
 @inject('setgegdluud', 'App\Setgegdel')
+@if(count($setgegdluud->getLatest()))
+
 <div class="row no-gutters justify-content-md-center setgegdel">
 	<div class="col-md-6">
 		<h3 class="setgegdel-title">Сэтгэгдэл</h3>
-		{!!$setgegdluud->getLatest()->body!!}
-		<span class="author">{{$setgegdluud->getLatest()->name}}</span>
+			{!!$setgegdluud->getLatest()->body!!}
+			<span class="author">{{$setgegdluud->getLatest()->name}}</span>
 	</div>
 	<img class="left1"  src="/assets/setgegdel/{{$setgegdluud->getLatest()->image1}}">
 	<img class="left2"  src="/assets/setgegdel/{{$setgegdluud->getLatest()->image2}}">
 	<img class="right1" src="/assets/setgegdel/{{$setgegdluud->getLatest()->image3}}">
 	<img class="right2" src="/assets/setgegdel/{{$setgegdluud->getLatest()->image4}}">
 </div>
+@endif
